f187605de2b628fe2d3213ea8d4a000e6d741381
iliameeqvse <dzodzuashviliilia787l@gmail.com>
2026-04-07 17:37:56 +0400

    swaggeri da jwt

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-
 from .views import register_view, login_view, me_view
 
 urlpatterns = [
     path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("me/", me_view, name="me"),
+    path("login/",    login_view,    name="login"),
+    path("me/",       me_view,       name="me"),
 ]
